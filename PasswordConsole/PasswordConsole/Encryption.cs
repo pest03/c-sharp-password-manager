09c9236c613d786d8a880716ffc557787e573881
pest03 <lopatarjirka@seznam.cz>
2018-06-23 09:27:21 +0200

    C# password manager
    Not very well done, but secure.

---
+++ PasswordConsole/PasswordConsole/Encryption.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Security.Cryptography;
+namespace PasswordConsole
+{
+    class Encryption
+    {
+
+        private static byte[] AES_Encrypt(byte[] bytesToBeEncrypted, byte[] passwordBytes)
+        {
+            byte[] encryptedBytes = null;
+
+
+            byte[] saltBytes = new byte[] { 0x64, 0x86, 0x32, 1, 2, 4, 7, 11 };
+
+            using (MemoryStream ms = new MemoryStream())
+            {
+                using (RijndaelManaged AES = new RijndaelManaged())
+                {
+                    AES.KeySize = 256;
+                    AES.BlockSize = 128;
+
+                    var key = new Rfc2898DeriveBytes(passwordBytes, saltBytes, 1000);
+                    AES.Key = key.GetBytes(AES.KeySize / 8);
+                    AES.IV = key.GetBytes(AES.BlockSize / 8);
+
+                    AES.Mode = CipherMode.CBC;
+
+                    using (var cs = new CryptoStream(ms, AES.CreateEncryptor(), CryptoStreamMode.Write))
+                    {
+                        cs.Write(bytesToBeEncrypted, 0, bytesToBeEncrypted.Length);
+                        cs.Close();
+                    }
+                    encryptedBytes = ms.ToArray();
+                }
+            }
+
+            return encryptedBytes;
+        }
+        private static byte[] AES_Decrypt(byte[] bytesToBeDecrypted, byte[] passwordBytes)
+        {
+            byte[] decryptedBytes = null;
+
+            // Set your salt here, change it to meet your flavor:
+            // The salt bytes must be at least 8 bytes.
+
+            byte[] saltBytes = new byte[] { 0x64, 0x86, 0x32, 1, 2, 4, 7, 11 };
+
+            using (MemoryStream ms = new MemoryStream())
+            {
+                using (RijndaelManaged AES = new RijndaelManaged())
+                {
+                    AES.KeySize = 256;
+                    AES.BlockSize = 128;
+
+                    var key = new Rfc2898DeriveBytes(passwordBytes, saltBytes, 1000);
+                    AES.Key = key.GetBytes(AES.KeySize / 8);
+                    AES.IV = key.GetBytes(AES.BlockSize / 8);
+
+                    AES.Mode = CipherMode.CBC;
+
+                    using (var cs = new CryptoStream(ms, AES.CreateDecryptor(), CryptoStreamMode.Write))
+                    {
+                        cs.Write(bytesToBeDecrypted, 0, bytesToBeDecrypted.Length);
+                        cs.Close();
+                    }
+                    decryptedBytes = ms.ToArray();
+                }
+            }
+
+            return decryptedBytes;
+        }
+        public static string EncryptText(string input, string password)
+        {
+
+            byte[] bytesToBeEncrypted = Encoding.UTF8.GetBytes(input);
+            byte[] passwordBytes = Encoding.UTF8.GetBytes(password);
+
+
+            passwordBytes = SHA256.Create().ComputeHash(passwordBytes);
+
+            byte[] bytesEncrypted = AES_Encrypt(bytesToBeEncrypted, passwordBytes);
+
+            string result = Convert.ToBase64String(bytesEncrypted);
+
+            return result;
+        }
+        public static  string DecryptText(string input, string password)
+        {
+            // Get the bytes of the string
+            byte[] bytesToBeDecrypted = Convert.FromBase64String(input);
+            byte[] passwordBytes = Encoding.UTF8.GetBytes(password);
+            passwordBytes = SHA256.Create().ComputeHash(passwordBytes);
+
+            byte[] bytesDecrypted = AES_Decrypt(bytesToBeDecrypted, passwordBytes);
+
+            string result = Encoding.UTF8.GetString(bytesDecrypted);
+
+            return result;
+        }
+        public static string Full_Encrypt(string keY, string salt, string to_encrypt)
+        {
+            return EncryptText(ComputeHash(CreateMD5(to_encrypt), Encoding.ASCII.GetBytes(salt)), keY);
+        }
+
+        public static string CreateMD5(string input)
+        {
+
+            using (System.Security.Cryptography.MD5 md5 = System.Security.Cryptography.MD5.Create())
+            {
+                byte[] inputBytes = System.Text.Encoding.ASCII.GetBytes(input);
+                byte[] hashBytes = md5.ComputeHash(inputBytes);
+
+
+                StringBuilder sb = new StringBuilder();
+                for (int i = 0; i < hashBytes.Length; i++)
+                {
+                    sb.Append(hashBytes[i].ToString("X2"));
+                }
+                return sb.ToString();
+            }
+        }
+        private static string ComputeHash(string plainText,
+                                    byte[] saltBytes)
+        {
+
+            if (saltBytes == null)
+            {
+
+                int minSaltSize = 4;
+                int maxSaltSize = 8;
+
+
+                Random random = new Random();
+                int saltSize = random.Next(minSaltSize, maxSaltSize);
+
+
+                saltBytes = new byte[saltSize];
+
+
+                RNGCryptoServiceProvider rng = new RNGCryptoServiceProvider();
+
+
+                rng.GetNonZeroBytes(saltBytes);
+            }
+
+
+            byte[] plainTextBytes = Encoding.UTF8.GetBytes(plainText);
+
+
+            byte[] plainTextWithSaltBytes =
+                    new byte[plainTextBytes.Length + saltBytes.Length];
+
+
+            for (int i = 0; i < plainTextBytes.Length; i++)
+                plainTextWithSaltBytes[i] = plainTextBytes[i];
+
+
+            for (int i = 0; i < saltBytes.Length; i++)
+                plainTextWithSaltBytes[plainTextBytes.Length + i] = saltBytes[i];
+
+
+            HashAlgorithm hash;
+
+
+
+
+            hash = new SHA512Managed();
+
+
+            byte[] hashBytes = hash.ComputeHash(plainTextWithSaltBytes);
+
+            byte[] hashWithSaltBytes = new byte[hashBytes.Length +
+                                                saltBytes.Length];
+
+            for (int i = 0; i < hashBytes.Length; i++)
+                hashWithSaltBytes[i] = hashBytes[i];
+
+            for (int i = 0; i < saltBytes.Length; i++)
+                hashWithSaltBytes[hashBytes.Length + i] = saltBytes[i];
+            string hashValue = Convert.ToBase64String(hashWithSaltBytes);
+
+
+            return hashValue;
+        }
+
+        
+    }
+}
---
+++ PasswordConsole/PasswordConsole/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.IO;
+using System.Diagnostics;
+namespace PasswordConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             string path = AppDomain.CurrentDomain.BaseDirectory;
+
+            if (File.Exists(path + "n5Ur5vfbHWmKMpRS.dat") && File.Exists(path + "F5SnLw9buNKKNYqx.dat"))
+            {
+                Console.WriteLine("Please enter password you will be using for unlocking the passwords:");
+                string password = Console.ReadLine();
+                File.WriteAllText(path + "VaFS3DNg5myqK87V.dat", Encryption.Full_Encrypt("sfnsjkdfscvsd1f3srg1sv3x", "se4ts9gsbdfbdfh", password));
+                File.WriteAllText(path + "j5aPSX7MMWcGrXkq.dat", Encryption.CreateMD5(password));
+                File.Create(path + "exeBNFwsN3An97hA.dat"); //passwords file
+                File.Delete("F5SnLw9buNKKNYqx.dat");
+                Process.Start(AppDomain.CurrentDomain.FriendlyName);
+                Environment.Exit(0);
+
+            }
+            else if (File.Exists(path + "n5Ur5vfbHWmKMpRS.dat") && !File.Exists(path + "F5SnLw9buNKKNYqx.dat"))
+            {
+                Console.WriteLine("Please type in your master password to unlock your passwords:");
+                string password = Console.ReadLine();
+                if (Encryption.Full_Encrypt("sfnsjkdfscvsd1f3srg1sv3x", "se4ts9gsbdfbdfh", password) == File.ReadAllText(path + "VaFS3DNg5myqK87V.dat"))
+                {
+                    Console.Clear();
+                    Console.WriteLine("Please choose option by typing its number");
+                    Console.WriteLine("Please keep in mind that if you have not entered any passwords yet, you need to insert them first.");
+                    Console.WriteLine("[1] Insert passwords");
+                    Console.WriteLine("[2] Read passwords");
+                    
+                    string response = Console.ReadLine();
+                    switch (response)
+                    {
+                        case "1":
+                            {
+                                Console.Clear();
+                                Console.WriteLine("Please insert your passwords to securely encrypt them.");
+                                Console.WriteLine("This will overwrite whole password file, if you are adding more please count with your old ones too");
+                                string passwords = Console.ReadLine();
+                                if (passwords != "")
+                                {
+                                    File.WriteAllText(path + "exeBNFwsN3An97hA.dat", Encryption.EncryptText(passwords, Encryption.EncryptText(Encryption.CreateMD5(File.ReadAllText(path + "j5aPSX7MMWcGrXkq.dat") + password), "E4wDyt92QaEphrV2")));
+                                    Console.Clear();
+                                    Console.WriteLine("Passwords written, press any key to restart the password manager and view them.");
+                                    Console.ReadLine();
+                                    Process.Start(AppDomain.CurrentDomain.FriendlyName);
+                                    Environment.Exit(0);
+                                }
+                                break;
+                            }
+                        case "2":
+                            {
+                                Console.Clear();
+                                Console.Write(Encryption.DecryptText(File.ReadAllText(path + "exeBNFwsN3An97hA.dat"), Encryption.EncryptText(Encryption.CreateMD5(File.ReadAllText(path + "j5aPSX7MMWcGrXkq.dat") + password), "E4wDyt92QaEphrV2")));
+                                Console.Read();
+                                break;
+                            }
+
+                    }
+                    Console.Clear();
+
+                }
+                else
+                {
+                    Console.WriteLine("Wrong password entered, please restart me and check out.");
+                    Console.Read();
+                }
+            }
+            else
+            {
+
+                Console.WriteLine("Preparing for first run..");
+                File.Create(path + "VaFS3DNg5myqK87V.dat"); //password file       
+                File.Create(path + "j5aPSX7MMWcGrXkq.dat"); //password md5 file
+                File.Create(path + "F5SnLw9buNKKNYqx.dat"); //has choosen password
+                File.Create(path + "n5Ur5vfbHWmKMpRS.dat"); //first start happened.
+                Console.WriteLine("Everything setup, let me restart in a second.");
+                Process.Start(AppDomain.CurrentDomain.FriendlyName);
+                Environment.Exit(0);
+            }
+
+        }
+    }
+}
